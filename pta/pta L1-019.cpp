d719af8483364a8019c33cfec45654714d1583d7
贵权 钟 <3124126720@qq.com>
2023-04-25 21:08:44 +0800

    pta L1=019

--- pta/pta L1-019.cpp
+++ pta/pta L1-019.cpp
@@ -1,49 +1,35 @@
 #include<iostream>
 using namespace std;
-
-
-
 int main()
 {
-	int a_d = 0, b_d = 0;
-	int a, b;
-	cin >> a >> b;
-	int n;
-	cin >> n;
-	int aa = 0, bb = 0;
-	int u,v, x, y;
+	int a_d = 0, b_d = 0, aa = 0, bb = 0;;
+	int a, b, n;
+	cin >> a >> b >> n;
+	int u, v, x, y;
 	while (n--)
 	{
 		cin >> u >> v >> x >> y;
-		if (((v == u + x) && (y = u + x))||((v!=u+x)&&(y!=u+x)))
-		{
-			continue;
-		}
-		if (v == u + x)
+
+		if (v == u + x && y != v)
 		{
 			aa++;
-			a_d++;
 		}
-		if (y == u + x)
+		if (y == u + x && v != y)
 		{
 			bb++;
-			b_d++;
 		}
 		if (aa > a)
 		{
-			cout << "B" << endl;
-			cout << b_d << endl;
+			cout << "A" << endl;
+			cout << bb << endl;
 			break;
 		}
 		if (bb > b)
 		{
-			cout << "A" << endl;
-			cout<<a_d<<endl;
+			cout << "B" << endl;
+			cout << aa << endl;
 			break;
 		}
-
 	}
-
-	system("pause");
 	return 0;
 }
